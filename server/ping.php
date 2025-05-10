59d70dcece8b314de399d7a9e41dec96a61cd3b9
tinagrit <mu@tinagrit.com>
2025-05-10 16:01:20 +0700

    pinging

---
+++ server/ping.php
@@ -0,0 +1,45 @@
+<?php 
+    // Device will send a HTTP request to this URL on every specified interval
+
+    const STAMPS = __DIR__ . "/stamps.json";
+    const DEVICES = __DIR__ . "/devices.json";
+
+    if (!isset($_GET['id'])) {
+        http_response_code(400);
+        exit("Device ID not given");
+    }
+
+    $id = $_GET['id'];
+
+    if (!ctype_digit($id) || (int)$id < 1 || (int)$id > 9999) {
+        http_response_code(400);
+        exit("Device ID invalid");
+    }
+
+    if (!file_exists(STAMPS)) {
+        http_response_code(503);
+        exit("Internal Server Error");
+    }
+
+    $stamps = file_get_contents(STAMPS);
+    $stampsParsed = json_decode($stamps);
+
+    $stampsParsed->{$_GET['id']} = time();
+
+    $stampsOutput = json_encode($stampsParsed);
+    file_put_contents(STAMPS,$stampsOutput);
+
+    $devices = file_get_contents(DEVICES);
+    $devicesParsed = json_decode($devices);
+    if (isset($devicesParsed->{(string)$id})) {
+
+        if ($devicesParsed->{(string)$id}->{"notified"} == true) {
+            $devicesParsed->{(string)$id}->{"notified"} = false;
+            $devicesOutput = json_encode($devicesParsed);
+            file_put_contents(DEVICES,$devicesOutput);
+        } 
+    }
+
+    http_response_code(200);
+    exit("Success");
+?>
